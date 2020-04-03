c9babb10c60f3ac46b2756bf79b61c27f860ac61
Rijin Mk <rijin.mk9@gmail.com>
2020-04-03 19:03:35 +0400

    Designing map color

---
+++ public/js/main.js
@@ -0,0 +1,117 @@
+let covid;
+let news = {};
+$.get({
+    url: '/covid',
+    success: (data) => {
+        covid = data.data;
+        let countries = data.countries;
+        // renderMaps(covid);
+
+        let whenCalls = ``
+
+        for (let i in countries) {
+            whenCalls += `
+                        $.get({
+                            url: "/news?country=${countries[i]}",
+                            success: (data) => {
+                                try{
+                                    news["${countries[i]}"] = {img: data[0].urlToImage, desc:data[0].title};
+                                }catch(e){
+                                    news["${countries[i]}"] = "";
+                                }
+                            }
+                        }),`
+        }
+
+        whenCalls = whenCalls.slice(0, -1);
+        // console.log(whenCalls);
+
+        $.when(
+            eval(whenCalls)
+        ).then(() => {
+            renderMaps(covid, news);
+        });
+
+    }
+});
+
+function renderMaps(covid) {
+    // console.log(covid, news);
+    console.log("------------------------------------");
+    console.log(covid);
+    console.log("------------------------------------");
+    console.log(news);
+    console.log("------------------------------------");
+    for (let i = 1; i < covid.length - 1; i++) {
+        console.log(covid[i]); 
+        covid[i][2] = `<h5>Infected: ${covid[i][1]}</h5>`;
+        covid[i][2] += `<p>${news[covid[i][0]].desc}</p>`;
+    }
+    google.charts.load('current', {
+        'packages': ['geochart'],
+        // Note: you will need to get a mapsApiKey for your project.
+        // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+        'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+    });
+    google.charts.setOnLoadCallback(drawRegionsMap);
+
+    function drawRegionsMap() {
+        $('.titles img').fadeOut(); 
+        var data = google.visualization.arrayToDataTable(covid);
+
+        let colors = []; 
+        function hslToHex(h, s, l) {
+            h /= 360;
+            s /= 100;
+            l /= 100;
+            let r, g, b;
+            if (s === 0) {
+                r = g = b = l; // achromatic
+            } else {
+                const hue2rgb = (p, q, t) => {
+                    if (t < 0) t += 1;
+                    if (t > 1) t -= 1;
+                    if (t < 1 / 6) return p + (q - p) * 6 * t;
+                    if (t < 1 / 2) return q;
+                    if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+                    return p;
+                };
+                const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+                const p = 2 * l - q;
+                r = hue2rgb(p, q, h + 1 / 3);
+                g = hue2rgb(p, q, h);
+                b = hue2rgb(p, q, h - 1 / 3);
+            }
+            const toHex = x => {
+                const hex = Math.round(x * 255).toString(16);
+                return hex.length === 1 ? '0' + hex : hex;
+            };
+            return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+        }
+
+
+        // hsl(120, 92%, 69%)
+        for (let i = 68; i <= 355; i+=0.5) {
+            colors.push(hslToHex(i, 92, 69));
+        }
+
+        var options = {};
+        // options['region'] = 'US';
+        // options['resolution'] = 'provinces';
+        options['colorAxis'] = {
+            minValue: 100,
+            maxValue: 100000,
+            colors: colors
+        };
+        // options['backgroundColor'] = '#FFFFFF';
+        // options['datalessRegionColor'] = '#E5E5E5';
+        options.tooltip = {
+            isHtml: true
+        };
+        options.legend = 'none'; 
+
+        var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+
+        chart.draw(data, options);
+    }
+}
